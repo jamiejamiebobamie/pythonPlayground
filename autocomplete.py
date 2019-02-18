af778331f54ddc155029bf9addcf98e3b2e4b768
jamiejamiebobamie <jmccrory@vt.edu>
2019-02-17 21:21:12 -0800

    autocomplete trie function

--- autocomplete.py
+++ autocomplete.py
@@ -16,32 +16,46 @@ def dict():
 
 
 class Trie:
-    def __init__(self, value=None):
+    def __init__(self, value=None, i=None):
         self.value = value
         self.dict = {}
         self.end = False
-        self.count = 0
+        self.count = 0 # how many words contain this prefix
+        self.depth = i # how many letters into the word, 0 = first letter
 
 
 def buildTrie(array):
     root = Trie()
     current = root
     for word in array:
+        # print("word " + word + '\n')
         for i, char in enumerate(word):
             c = char.lower()
             if c not in current.dict:
-                current.dict[c] = Trie(c)
-                root.count += 1
+                # print('adding!')
+                current.dict[c] = Trie(c,i)
+            current.count += 1
             current = current.dict[c]
+            # print('char ' + current.value)
         current.end = True
+        # print(count, current.value, current.end)
         current = root
+    # print(root.count)
     return root
-
+#this works (tested)
 
 
 array = dict()
-print('jom' in array)
 root = buildTrie(array)
+# print(root)
+
+# current = root
+# count = 0
+# while current:
+#     count+=1
+#     print(current.depth, count, current.count)
+#     current = current.dict['a']
+
 
 # current = root.dict['a'].dict['u']
 # print(current.dict)
@@ -49,7 +63,7 @@ root = buildTrie(array)
 #     print(key)
 
 def findWords(pre, root):
-    alpha = list('abcdefghijklmnopqrstuvwxyz')
+    # alpha = list('abcdefghijklmnopqrstuvwxyz')
     # print(alpha)
     # def rec(pre, node):
     #     word = pre
@@ -66,22 +80,45 @@ def findWords(pre, root):
     word = pre
     words = []
     current = root
-    for char in pre:
+
+    for char in pre: #move up to the correct point in the dictionary
         if char in current.dict:
             current = current.dict[char]
-        else:
-            return 'no words exist in English that start like that'
+            print(char)
+        # else:
+        #     return 'no words exist in English that start like that'
 
-    known = current
-    for _ in range(len(alpha)):
-        # print(alpha[_],current.dict)
-        if alpha[_] in current.dict:
-            current = current.dict[alpha[_]]
-            word += current.value
-        else:
-            if current.end == True and word not in words:
-                words.append(word)
-            word = pre
+    known = current #the correct point in the tree based on the prefix
+    suffixCount = known.count
+    # print(suffixCount)
+
+    # while suffixCount > 0:
+
+    def goIn(node):
+        # depthChars = node.dict.keys()
+        for key in node.dict:
+            print(node.dict[key].value,node.dict[key].depth)
+            # w.append(node.dict[key].value)
+            # if node.dict[key].end == True:
+            #     break
+            goIn(node.dict[key])
+            # return node.dict[key].value
+
+    words.append(goIn(known))
+
+
+
+    #
+
+    # for _ in range(len(alpha)):
+    #     # print(alpha[_],current.dict)
+    #     if alpha[_] in current.dict:
+    #         current = current.dict[alpha[_]]
+    #         word += current.value
+    #     else:
+    #         if current.end == True and word not in words:
+    #             words.append(word)
+    #         word = pre
 
     #
     # while current.dict[_].end == False:
@@ -90,4 +127,4 @@ def findWords(pre, root):
     return words
 
 
-print(findWords('poo',root))
+print(findWords('aard', root))
