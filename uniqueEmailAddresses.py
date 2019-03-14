6d783f3a46acc0dbd14adbc536fc658651b8240d
jamiejamiebobamie <jmccrory@vt.edu>
2019-03-14 09:51:56 -0700

    unique email problem

--- uniqueEmailAddresses.py
+++ uniqueEmailAddresses.py
@@ -4,13 +4,20 @@ For example, in alice@leetcode.com, alice is the local name, and leetcode.com is
 
 Besides lowercase letters, these emails may contain '.'s or '+'s.
 
-If you add periods ('.') between some characters in the local name part of an email address, mail sent there will be forwarded to the same address without dots in the local name.  For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address.  (Note that this rule does not apply for domain names.)
+If you add periods ('.') between some characters in the local name part of an email address,
+mail sent there will be forwarded to the same address without dots in the local name.
+For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address.
+(Note that this rule does not apply for domain names.)
 
-If you add a plus ('+') in the local name, everything after the first plus sign will be ignored. This allows certain emails to be filtered, for example m.y+name@email.com will be forwarded to my@email.com.  (Again, this rule does not apply for domain names.)
+If you add a plus ('+') in the local name, everything after the first plus sign will be ignored.
+This allows certain emails to be filtered, for example m.y+name@email.com
+will be forwarded to my@email.com.
+(Again, this rule does not apply for domain names.)
 
 It is possible to use both of these rules at the same time.
 
-Given a list of emails, we send one email to each address in the list.  How many different addresses actually receive mails?
+Given a list of emails, we send one email to each address in the list.
+How many different addresses actually receive mails?
 
 
 
@@ -26,3 +33,33 @@ Note:
 1 <= emails[i].length <= 100
 1 <= emails.length <= 100
 Each emails[i] contains exactly one '@' character."""
+
+
+
+A = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+
+def howMany(A):
+    dict = {}
+    for e in A:
+        email = ""
+        local = True
+        plus = False
+        for char in e:
+            if local:
+                if char == '@':
+                    local = False
+                elif char == "+":
+                    plus = True
+                elif char == ".":
+                    continue
+                elif plus == False:
+                    email += char
+            else:
+                email += char
+        if email in dict:
+            dict[email] += 1
+        else:
+            dict[email] = 1
+    return len(dict)
+
+print(howMany(A))
