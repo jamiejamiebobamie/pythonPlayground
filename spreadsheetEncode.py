e8feef679f698a32e5badc58602cf036f3e69c77
jamiejamiebobamie <jmccrory@vt.edu>
2019-03-19 08:02:33 -0700

    added spreadsheetEncode.py

---
+++ spreadsheetEncode.py
@@ -0,0 +1,8 @@
+"""Implement a finction that converts a spreadsheet column id to the corresponding integer,
+with "A" coressponding to 1.
+For example you should return 4 for "D", 27 for "AA", 702 for "ZZ", etc.
+How would you test your code?"""
+
+
+def convert_Column_to_Int(column_id):
+    
