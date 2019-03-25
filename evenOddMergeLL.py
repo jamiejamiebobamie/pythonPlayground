d023b28f1ee8c96943ca1a0b54b6dcddfce86c12
jamiejamiebobamie <jmccrory@vt.edu>
2019-03-24 18:35:36 -0700

    added to evenOddMergeLL.py

--- evenOddMergeLL.py
+++ evenOddMergeLL.py
@@ -8,7 +8,7 @@ the nodes with odd-numbered data.
 
 
 class Node:
-    def __init__(self, data=0, next=None):
+    def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
 
@@ -20,12 +20,12 @@ class Node:
 # b = Node(1, c)
 # a = Node(0, b)
 
-h = Node(6)
+h = Node(7)
 g = Node(5, h)
 f = Node(5, g)
 e = Node(3, f)
 d = Node(3, e)
-c = Node(3, d)
+c = Node(2, d)
 b = Node(1, c)
 a = Node(1, b)
 
@@ -40,35 +40,46 @@ def iterateList(L):
     return array
 
 def merge(L):
-    bool = False
-    head = odd = odd_head = L
-
-
-    while head.data % 2 != 0:
-
-        odd = odd.next
-        head = head.next
-
-    print("print"+str(iterateList(odd)))
-
-
-    odd.next = None
-    new_head = head
-
-
-    while head.next:
-        if head.data % 2 == 0:
-            head = head.next
-        else:
-            odd.next = head
+    even_head = even = Node()
+    odd_head = odd = Node()
+    while L:
+        if L.data % 2 == 0:
+            even.next = L
+            even = even.next
+        if L.data % 2 != 0:
+            odd.next = L
             odd = odd.next
-            while odd.data % 2 != 0:
-                odd = odd.next
-        head.next = odd_head
+        L = L.next
+    else:
+        odd.next = None
+        even.next = odd_head
+    return even_head
+
+print(iterateList(merge(L)))
+
+
+
+
+    # print("print"+str(iterateList(even)))
+
+
+    # odd.next = None
+    # new_head = head
+    #
+    #
+    # while head.next:
+    #     if head.data % 2 == 0:
+    #         head = head.next
+    #     else:
+    #         odd.next = head
+    #         odd = odd.next
+    #         while odd.data % 2 != 0:
+    #             odd = odd.next
+    #     head.next = odd_head
 
     # print(iterateList(odd))
 
-    return new_head
+    # return new_head
 
-print(iterateList(L))
-print(iterateList(merge(L)))
+# print(iterateList(L))
+# print(iterateList(merge(L)))
