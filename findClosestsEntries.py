9e05d751aa168cb9cfb7ef838214c3fc278f86db
jamiejamiebobamie <jmccrory@vt.edu>
2019-04-04 08:43:46 -0700

    iterators to be aware of value gaps between the three arrays

--- findClosestsEntries.py
+++ findClosestsEntries.py
@@ -14,3 +14,42 @@ Output: [15,15,16]
 
 
 """
+
+import collections
+
+A1 = [5,10,15]
+A2 = [3,6,9,12,15]
+A3 = [8,16,24]
+
+def findThree(A1,A2,A3):
+    i = j = k = 0
+
+    result = collections.namedtuple('result',('A1','A2','A3','difference'))
+
+    result.A1 = A1[0]
+    result.A2 = A2[0]
+    result.A3 = A3[0]
+    result.difference = max(abs(result.A1-result.A3), max(abs(result.A1-result.A2),abs(result.A2-result.A3)))
+
+    while i < len(A1) or j < len(A2) or k < len(A3):
+        if i < len(A1):
+            if result.difference > max(abs(A1[i]-result.A3), max(abs(A1[i]-result.A2),abs(result.A2-result.A3))):
+                result.difference = max(abs(A1[i]-result.A3), max(abs(A1[i]-result.A2),abs(result.A2-result.A3)))
+                result.A1 = A1[i]
+            print("A1",A1[i])
+            i+=1
+        if j < len(A2):
+            if result.difference > max(abs(result.A1-result.A3), max(abs(result.A1-A2[j]),abs(A2[j]-result.A3))):
+                result.difference = max(abs(result.A1-result.A3), max(abs(result.A1-A2[j]),abs(A2[j]-result.A3)))
+                result.A2 = A2[j]
+            print("A2",A2[j])
+            j+=1
+        if k < len(A3):
+            if result.difference > max(abs(result.A1-A3[k]), max(abs(result.A1-result.A2),abs(result.A2-A3[k]))):
+                result.difference = max(abs(result.A1-A3[k]), max(abs(result.A1-result.A2),abs(result.A2-A3[k])))
+                result.A3 = A3[k]
+            print("A3",A3[k])
+            k+=1
+    return result.A1, result.A2, result.A3, result.difference
+
+print(findThree(A1,A2,A3))
