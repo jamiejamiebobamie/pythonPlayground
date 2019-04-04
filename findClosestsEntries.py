59d554da44e625731916c077560977f3c58ca787
jamiejamiebobamie <jmccrory@vt.edu>
2019-04-04 07:57:51 -0700

    added findClosestinterval.py

---
+++ findClosestsEntries.py
@@ -0,0 +1,16 @@
+"""
+Elements of Programmign Interviews in Python
+pg 210
+
+Design an algorithm that takes three sorted arrays and returns one entry from
+each such that the minimum interval containing these three entries is as small
+as possible.
+
+For example:
+Input: [5,10,15], [3,6,9,12,15], [8,16,24]
+Output: [15,15,16]
+
+( Interval: 1 =  abs(15-16) )
+
+
+"""
