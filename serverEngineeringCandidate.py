1a722780c3f266958bcc2ef018b9a3a6fab7bd60
jamiejamiebobamie <jmccrory@vt.edu>
2019-06-13 11:25:19 -0700

    added methods to control the size of the world relative to player population

--- serverEngineeringCandidate.py
+++ serverEngineeringCandidate.py
@@ -3,35 +3,56 @@
 import random
 
 class Dungeon:
-    def __init__(self,numberOfRooms):
-        self.numberOfRooms = numberOfRooms
+    def __init__(self,numberOfRooms=None,numberOfPlayers=1):
+        # self.numberOfRooms = numberOfRooms
         self.world = {}
-        # self.world[(0,0,0)] = self.Room(0, 0, 0, bool(random.getrandbits(1)))
-        self.nextRoomCoordinate = [1,0] # first item is the coordinate value, the second item denotes xyz (x=0,y=1,z=2)
-        self.current_coordinate = [0,0,0]
-        # self.instantiateDungeon()
+        self.n = 0
+        self.numberOfPlayers = numberOfPlayers
+        self.addRooms(numberOfRooms)
 
-    def instantiateDungeon(self):
-        for n in range(self.numberOfRooms):
-            self.world[(n,0,0)] = self.Room(0, 0, 0, bool(random.getrandbits(1)))
-            self.world[(0,n,0)] = self.Room(0, 0, 0, bool(random.getrandbits(1)))
-            self.world[(0,0,n)] = self.Room(0, 0, 0, bool(random.getrandbits(1)))
-            self.world[(n,n,0)] = self.Room(0, 0, 0, bool(random.getrandbits(1)))
-            self.world[(0,n,n)] = self.Room(0, 0, 0, bool(random.getrandbits(1)))
-            self.world[(n,n,n)] = self.Room(0, 0, 0, bool(random.getrandbits(1)))
+    def permute(self, a):
+        array = a
+        permutations = set()
+        def __helper(a, l, r):
+            if l==r:
+                perm = tuple(a)
+                permutations.add(perm)
+            else:
+                for i in xrange(l,r+1):
+                    a[l], a[i] = a[i], a[l]
+                    __helper(a, l+1, r)
+                    a[l], a[i] = a[i], a[l]
+            return permutations
+        return __helper(array, 0, 2)
 
-
-    def addRooms(self):
-        # add an xyz room coordinate and its positive/negative variations.
-        print(self.current_coordinate)
-        if self.nextRoomCoordinate[1] > 1:
-            self.nextRoomCoordinate[0] = 0
-            self.nextRoomCoordinate[1] = 0
+    def addRooms(self, number=None, x=None, y=None, z=None):
+        # builds out the world space creating 3d coordinates in a number x number x number block
+        if x == None or y == None or z == None:
+            x = y = z = self.n
+        if number == None:
+            number = self.numberOfPlayers
+        self.n = number
+        while x < number:
+            coordinate = [x,y,z]
+            for coord in self.permute(coordinate):
+                self.world[coord] = self.Room(coord[0], coord[1], coord[2], bool(random.getrandbits(1)))
+            x+=1
         else:
-            self.nextRoomCoordinate[1] += 1
-        # print(self.nextRoomCoordinate)
-        self.current_coordinate[self.nextRoomCoordinate[1]] = self.nextRoomCoordinate[0]
-
+            while y < number:
+                coordinate = [x,y,z]
+                for coord in self.permute(coordinate):
+                    self.world[coord] = self.Room(coord[0], coord[1], coord[2], bool(random.getrandbits(1)))
+                y+=1
+            else:
+                while z < number:
+                    coordinate = [x,y,z]
+                    for coord in self.permute(coordinate):
+                        self.world[coord] = self.Room(coord[0], coord[1], coord[2], bool(random.getrandbits(1)))
+                    z+=1
+                else:
+                    coordinate = [x,y,z]
+                    for coord in self.permute(coordinate):
+                        self.world[coord] = self.Room(coord[0], coord[1], coord[2], bool(random.getrandbits(1)))
 
     class Room:
         def __init__(self, x, y, z, transparent,description=None):
@@ -43,7 +64,7 @@ class Dungeon:
             self.players = {}
             self.items = {}
             self.monsters = {}
-            self.dialogue = LinkedList() #need to make.
+            self.dialogue = None#LinkedList() #need to make.
 
         def setDescription(self, userInput):
             if self.transparent == True and self.description == None:
@@ -65,131 +86,7 @@ class Dungeon:
             self.name = name
             self.description = description
 
-newDungeon = Dungeon(7)
-
-# print(len(newDungeon.world.keys()))
-# for key in newDungeon.world.keys():
-#     print(key)
-
-for _ in range(50):
-    newDungeon.addTwoRooms()
-
-#what room coordinates from -2 to 2 of unit size 1 look like:
-#
-# (0,0,0)
-#
-# (0,0,1)
-# (0,0,-1)
-#
-# (0,1,0)
-# (0,-1,0)
-#
-# (1,0,0)
-# (-1,0,0)
-#
-# (0,1,1)
-# (0,-1,-1)
-# (0,-1,1)
-# (0,1,-1)
-#
-# (1,1,0)
-# (-1,-1,0)
-# (1,-1,0)
-# (-1,1,0)
-#
-# (1,0,1)
-# (-1,0,1)
-# (1,0,-1)
-# (-1,0,-1)
-#
-# (1,1,1)
-# (-1,1,1)
-# (1,-1,1)
-# (1,1,-1)
-# (1,-1,-1)
-# (-1,-1,1)
-# (-1,1,-1)
-# (-1,-1,-1)
-#
-# (0,0,2)
-# (0,0,-2)
-#
-# (0,1,2)
-# (0,1,-2)
-# (0,-1,2)
-# (0,-1,-2)
-#
-# (1,0,2)
-# (1,0,-2)
-# (-1,0,2)
-# (-1,0,-2)
-#
-# (1,1,2)
-# (1,1,-2)
-# (1,-1,2)
-# (-1,1,2)
-# (1,-1,-2)
-# (-1,-1,2)
-# (-1,1,-2)
-# (-1,-1,-2)
-#
-# (2,0,0)
-# (-2,0,0)
-#
-# (2,1,0)
-# (-2,1,0)
-# (2,-1,0)
-# (-2,-1,0)
-#
-# (2,0,1)
-# (2,0,-1)
-# (-2,0,1)
-# (-2,0,-1)
-#
-# (0,2,0)
-# (0,-2,0)
-#
-# (1,2,0)
-# (-1,2,0)
-# (1,-2,0)
-# (-1,-2,0)
-#
-# (0,2,1)
-# (0,-2,1)
-# (0,2,-1)
-# (0,-2,-1)
-#
-# (1,2,1)
-# (-1,2,1)
-# (1,-2,1)
-# (1,2,-1)
-# (-1,-2,1)
-# (1,-2,-1)
-# (-1,-2,-1)
-#
-# (2,1,1)
-# (-2,1,1)
-# (2,-1,1)
-# (2,1,-1)
-# (2,-1,-1)
-# (-2,-1,1)
-# (-2,-1,-1)
-# (-2,1,1-)
-#
-# (2,2,1)
-# (-2,2,1)
-# (2,-2,1)
-# (2,2,-1)
-# (-2,-2,1)
-# (2,-2,-1)
-# (-2,2,-1)
-# (-2,-2,-1)
-#
-# (2,2,2)
-# (2,2,-2)
-# (2,-2,2)
-# (-2,2,2)
-# (2,-2,-2)
-# (-2,-2,2)
-# (-2,2,-2)
-# (-2,-2,-2)
+newDungeon = Dungeon(None, 1)
+print(len(newDungeon.world.keys()))
+for key in newDungeon.world.keys():
+    print(key)
